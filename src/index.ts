1b6f557d94ba1c994ccea55390f73d7cfdd905f2
danielafernandeztdf <104040499+danielafernandeztdf@users.noreply.github.com>
2022-04-25 20:37:17 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let precioInicial: number = 450.50;
+let descuento: number = precioInicial * 0.1;
+let precioFinal: number = (precioInicial - descuento);
+
+console.log ("el precioInicial del producto es de", precioInicial)
+console.log ("el descuento es de", descuento)
+console.log ("el resultado del precioFinal es de", precioFinal)
